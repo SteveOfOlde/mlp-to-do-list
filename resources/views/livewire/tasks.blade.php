965abfd61397a599a4fc9057568272a38a4eb452
SteveOfOlde <1040521+SteveOfOlde@users.noreply.github.com>
2025-08-21 13:12:21 +0100

    [COMPOSER INSTALL] hello world for livewire

---
+++ resources/views/livewire/tasks.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <p>component here</p>
+
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,10 +9,12 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    @livewireStyles
 </head>
 <body>
 
-<h1>Hello World</h1>
+<livewire:tasks />
 
+@livewireScripts
 </body>
 </html>
