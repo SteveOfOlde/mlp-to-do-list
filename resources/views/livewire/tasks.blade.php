3c90a06dfb976fcd52fc4a92a1622a34f3ffa833
SteveOfOlde <1040521+SteveOfOlde@users.noreply.github.com>
2025-08-21 14:15:43 +0100

    add simple bootstrap to tasks in a table

--- resources/views/livewire/tasks.blade.php
+++ resources/views/livewire/tasks.blade.php
@@ -4,9 +4,38 @@ use Illuminate\Database\Eloquent\Collection;
 /** @var $tasks Collection<Task> */
 ?>
 <div>
-    @isset($tasks)
-        @foreach ($tasks as $task)
-            <p>Task : {{ $task->title }}</p>
-        @endforeach
-    @endisset
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                @isset($tasks)
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Task</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tasks as $key=>$task)
+                                <tr>
+                                    <td>{{$key + 1}}</td>
+                                    <td>{{ $task->title }}</td>
+                                    <td>
+                                        @if(!$task->completed)
+                                            <button type="button" class="btn-btn-success"></button>
+                                            <button type="button" class="btn-btn-success"></button>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endisset
+            </div>
+        </div>
+    </div>
+
+
 </div>
